ba1d09cf471c1067c64d3102a92e2e7badf745b9
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 16:53:07 +0200

    Module 3 : Pratiquer le PHP en exercice, E2-Exo1 - E1 Tables de multiplication

--- coursPHP/Exo3/exo1.php
+++ coursPHP/Exo3/exo1.php
@@ -3,7 +3,15 @@
     include("common/menu.php");
 ?>
 
-<h1>Le premier exercice</h1>
+<h1>Affichage des tables de multiplication</h1>
+
+<?php
+    define("TABLE", 7);
+    echo "<h2> La table de " . TABLE . "</h2>";
+    for($i = 1; $i <= 10 ; $i++) {
+        echo $i . " * " . TABLE . " = " . $i * TABLE . "<br />";
+    }
+?>
 
 <?php 
     include("common/footer.php");
