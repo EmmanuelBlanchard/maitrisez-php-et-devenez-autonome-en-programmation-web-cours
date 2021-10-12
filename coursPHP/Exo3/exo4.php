2e2233e140c8ccae099ace76f7033705ef3bcc19
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 19:23:24 +0200

    Module 3 : Pratiquer le PHP en exercice, E7-Exo4 - E2 Utiliser les SESSIONs

--- coursPHP/Exo3/exo4.php
+++ coursPHP/Exo3/exo4.php
@@ -2,11 +2,18 @@
     include("common/header.php");
     include("common/menu.php");
 
-    $nombreAleatoire = 5;
+    session_start();
+    if(!isset($_SESSION["nombreAleatoire"])) {
+        $_SESSION["nombreAleatoire"] = rand(1,1000);
+    }
 ?>
 
 <h1>Trouver le nombre choisi par l'ordinateur</h1>
 
+<form action="#" method="post">
+    <input type="hidden" name="reinitialisation" value="yes">
+    <input type="submit" value="Reinitialiser">
+</form>
 <form action="#" method="post">
     <label for="nombreAleatoire">Quel est le nombre : </label>
     <input type="number" name="nombreAleatoire" id="nombreAleatoire">
@@ -15,6 +22,11 @@
 </form>
 
 <?php
+    if(isset($_POST["reinitialisation"]) && $_POST["reinitialisation"] === "yes") {
+        $_SESSION["nombreAleatoire"] = rand(1,1000);
+    }
+    $nombreAleatoire = $_SESSION["nombreAleatoire"];
+    
     if(isset($_POST["nombreAleatoire"]) && $_POST["nombreAleatoire"] > 0 ) {
         $nombreSaisi = (int)$_POST["nombreAleatoire"];
         echo "<h2>";
