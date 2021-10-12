0062d9bdb0a9c6136a069049701a7225ae48a5da
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 16:27:34 +0200

    Module 2 : HTML/CSS/JavaScript et PHP, E7-Realiser un menu pour disposer de plusieurs pages HTML

--- coursPHP/Exo2/exo2.php
+++ coursPHP/Exo2/exo2.php
@@ -1,41 +1,31 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="exo2.css">
-</head>
-<body>
-    <h1 class="titreBleu"> Formulaire </h1>
-    <form action="#" method="post">
-        <fieldset>
-            <legend>Informations</legend>
-            <label for="nom">Nom : </label>
-            <input type="text" name="nom" id="nom" required>
-            <label for="age">Age : </label>
-            <input type="number" name="age" id="age" required>
-            <input type="submit" value="Envoyer">
-        </fieldset>
-    </form>
-    <?php 
-        // if(isset($_GET["nom"])) {
-        //     echo "<div class=\"resultat\">";
-        //     echo "Le nom est : " . $_GET["nom"] . "<br />";
-        //     if(isset($_GET["age"])) {
-        //         echo "L'age est : " . $_GET["age"] . "<br />";
-        //     }
-        //     echo "<div>";
-        // }
-        if(isset($_POST["nom"])) {
-            echo "<div class=\"resultat\">";
-            echo "Le nom est : " . $_POST["nom"] . "<br />";
-            if(isset($_POST["age"])) {
-                echo "L'age est : " . $_POST["age"] . "<br />";
-            }
-            echo "<div>";
+<?php 
+    include("common/header.php");
+    include("common/menu.php");
+?>
+
+<h1 class="titreBleu"> Formulaire </h1>
+<form action="#" method="post">
+    <fieldset>
+        <legend>Informations</legend>
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom" id="nom" required>
+        <label for="age">Age : </label>
+        <input type="number" name="age" id="age" required>
+        <input type="submit" value="Envoyer">
+    </fieldset>
+</form>
+
+<?php
+    if(isset($_POST["nom"])) {
+        echo "<div class=\"resultat\">";
+        echo "Le nom est : " . $_POST["nom"] . "<br />";
+        if(isset($_POST["age"])) {
+            echo "L'age est : " . $_POST["age"] . "<br />";
         }
-    ?>
-</body>
-</html>
+        echo "<div>";
+    }
+?>
+
+<?php 
+    include("common/footer.php");
+?>
