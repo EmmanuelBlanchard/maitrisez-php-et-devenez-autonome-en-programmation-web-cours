6aed83ad2a74b17eb92f362f0908bc9a580a85b8
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 17:03:57 +0200

    Module 3 : Pratiquer le PHP en exercice, E3-Exo1 - E2 Tables de multiplication

--- coursPHP/Exo3/exo1.php
+++ coursPHP/Exo3/exo1.php
@@ -5,11 +5,20 @@
 
 <h1>Affichage des tables de multiplication</h1>
 
+<form action="#" method="get">
+    <label for="table">Table de multiplication : </label>
+    <input type="number" name="table" id="table">
+    <input type="submit" value="Envoyer">
+</form>
+
 <?php
-    define("TABLE", 7);
-    echo "<h2> La table de " . TABLE . "</h2>";
-    for($i = 1; $i <= 10 ; $i++) {
-        echo $i . " * " . TABLE . " = " . $i * TABLE . "<br />";
+    if(isset($_GET["table"])) {
+        echo "<h2> La table de " . $_GET["table"] . "</h2>";
+        for($i = 1; $i <= 10 ; $i++) {
+            echo $i . " * " . $_GET["table"] . " = " . $i * $_GET["table"] . "<br />";
+        }
+    } else {
+        echo "<h2>Saisir une valeur dans le champ ci-dessus</h2>";
     }
 ?>
 
