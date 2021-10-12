e424ad7be0f38bbc80fbf91b5adedeb609156468
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 11:54:53 +0200

    Module 1 : Les bases du PHP,E6-Les tests (if)

--- coursPHP/Exo1/maPage.php
+++ coursPHP/Exo1/maPage.php
@@ -7,32 +7,40 @@ $nomJoueur2 = "tata";
 $ageJoueur2 = 25;
 $estUnHommeJoueur2 = false;
 
-$text = test(3);
-echo $text;
-
+generationSeparation();
 afficherJoueur($nomJoueur1,$ageJoueur1,$estUnHommeJoueur1);
+generationSeparation();
 afficherJoueur($nomJoueur2,$ageJoueur2,$estUnHommeJoueur2);
 
+generationSeparation();
+afficherJoueurLePlusAge($ageJoueur1, $ageJoueur2);
+generationSeparation();
+
 function afficherJoueur($nom, $age, $homme) {
-    echo "Nom du joueur 1 : " . $nom;
+    echo "Nom du joueur : " . $nom;
     echo "<br />";
-    echo "Age du joueur 1 : " . $age;
-    $age = $age + 1;
-    echo "<br />";
-    echo "Age du joueur 1 : " . $age;
+    echo "Age du joueur : " . $age;
     echo "<br />";
 
-    if($homme === true) {
+    if($homme) {
         echo "C'est un homme";
     } else { //$estUnHomme === false
         echo "C'est une femme";
     }
 }
 
-function test($c) {
-    $a = 5;
-    $b = 10;
-    return $a + $b + $c;
+function afficherJoueurLePlusAge($ageJoueur1, $ageJoueur2) {
+    if($ageJoueur1 > $ageJoueur2) {
+        echo "Joueur 1 est le plus agé";
+    } else {
+        echo "Joueur 2 est le plus agé";
+    }
+}
+
+function generationSeparation() {
+    echo "<br />";
+    echo "----------------------";
+    echo "<br />";
 }
 
 ?>
