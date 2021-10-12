5ff8d1c604dcb4aba49a1df332c9c408d3d17c47
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-12 15:48:15 +0200

    Module 2 : HTML/CSS/JavaScript et PHP, E5-Les formulaires

--- coursPHP/Exo2/exo2.php
+++ coursPHP/Exo2/exo2.php
@@ -8,17 +8,16 @@
     <link rel="stylesheet" type="text/css" media="screen" href="exo2.css">
 </head>
 <body>
-    <h1 class="titreBleu"> Coucou </h1>
-    <h1 class="titreRouge"> Salut </h1>
-    <div class="backgroundColorRed">Un texte dans un paragraphe</div>
-    <div class="backgroundColorBlue"> 
-        <p> Bonjour </p>
-        <p> Salut </p>
-    </div>
-    <img src="sources/images/player.png" alt="image du personnage">
-
-    <input class="updatecolor" type="button" type="button" value="Changer la couleur">
-    
-    <script src="main.js"></script>
+    <h1 class="titreBleu"> Formulaire </h1>
+    <form action="#">
+        <fieldset>
+            <legend>Informations</legend>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" required>
+            <label for="age">Age : </label>
+            <input type="number" name="age" id="age" required>
+            <input type="submit" value="Envoyer">
+        </fieldset>
+    </form>
 </body>
 </html>
