62db72e31f30186077af6aeecb67d51fd13e5997
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 15:50:26 +0200

    Module 5 : La POO en exercice, E2-Exo2 -E2 Une liste de fruits, correction

--- coursPHP/Exo4/classes/fruits.class.php
+++ coursPHP/Exo4/classes/fruits.class.php
@@ -7,6 +7,9 @@ class Fruit {
     const POMME = "pomme";
     const CERISE = "cerise";
     
+    const POMME_IMAGE = "apple.png";
+    const CERISE_IMAGE = "cherry.png";
+
     public function __construct($nom,$poids) {
         $this->nom = $nom;
         $this->poids = $poids;
@@ -18,6 +21,22 @@ class Fruit {
             return 15;
         } else if($nom === self::CERISE) {
             return 20;
+        }
+    }
+
+    public function __toString() {
+        $affichage = $this->getAffichageImage();
+        $affichage .= "Nom : " . $this->nom . "<br />";
+        $affichage .= "Poids : " . $this->poids . "<br />";
+        $affichage .= "Prix : " . $this->prix . "<br />";
+        return $affichage;
+    }
+
+    private function getAffichageImage() {
+        if($this->nom === self::POMME) {
+            return "<img src=\"sources/images/".self::POMME_IMAGE."\" alt=\"image pomme\" /> <br/>";
+        } else if($this->nom === self::CERISE) {
+            return "<img src=\"sources/images/".self::CERISE_IMAGE."\" alt=\"image cerise\" /> <br/>"; 
         }
     }
 
--- coursPHP/Exo4/exo2.php
+++ coursPHP/Exo4/exo2.php
@@ -6,9 +6,20 @@
 
 <h1> Fruits : </h1>
 
-<?php 
+<?php
     $pomme1 = new Fruit(Fruit::POMME,150);
-    print_r($pomme1);
+    $pomme2 = new Fruit(Fruit::POMME,130);
+    $pomme3 = new Fruit(Fruit::POMME,160);
+    $cerise1 = new Fruit(Fruit::CERISE,10);
+    $cerise2 = new Fruit(Fruit::CERISE,15);
+    $cerise3 = new Fruit(Fruit::CERISE,20);
+    $cerise4 = new Fruit(Fruit::CERISE,10);
+    $fruits = [$pomme1,$pomme2,$pomme3,$cerise1,$cerise2,$cerise3,$cerise4];
+
+    foreach($fruits as $fruit){
+        echo $fruit;
+        echo "<br />------------------------ <br />";
+    }
 ?>
 
 <?php 
