f33424c0b3e02a1d8ca5e00cbc7d22b54f09a50c
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 15:36:34 +0200

    Module 5 : La POO en exercice, E1-Exo2 -E1 Une liste de fruits, correction

--- coursPHP/Exo4/exo2.php
+++ coursPHP/Exo4/exo2.php
@@ -7,24 +7,8 @@
 <h1> Fruits : </h1>
 
 <?php 
-$fruit1 = new Fruit(Fruit::POMME,Fruit::POMME_IMAGE,150,Fruit::POMME_PRIX);
-$fruit2 = new Fruit(Fruit::POMME,Fruit::POMME_IMAGE,130,Fruit::POMME_PRIX);
-$fruit3 = new Fruit(Fruit::POMME,Fruit::POMME_IMAGE,140,Fruit::POMME_PRIX);
-$fruit4 = new Fruit(Fruit::POMME,Fruit::POMME_IMAGE,145,Fruit::POMME_PRIX);
-
-$fruit5 = new Fruit(Fruit::CERISE,Fruit::CERISE_IMAGE,20,Fruit::CERISE_PRIX);
-$fruit6 = new Fruit(Fruit::CERISE,Fruit::CERISE_IMAGE,35,Fruit::CERISE_PRIX);
-$fruit7 = new Fruit(Fruit::CERISE,Fruit::CERISE_IMAGE,25,Fruit::CERISE_PRIX);
-$fruit8 = new Fruit(Fruit::CERISE,Fruit::CERISE_IMAGE,25,Fruit::CERISE_PRIX);
-$fruit9 = new Fruit(Fruit::CERISE,Fruit::CERISE_IMAGE,25,Fruit::CERISE_PRIX);
-
-$fruits = Fruit::getListeFruit();
-
-foreach($fruits as $fruit) {
-    $fruit->afficherMesInformationsModele();
-    echo "<br/>------------------------<br/>";
-}
-
+    $pomme1 = new Fruit(Fruit::POMME,150);
+    print_r($pomme1);
 ?>
 
 <?php 
