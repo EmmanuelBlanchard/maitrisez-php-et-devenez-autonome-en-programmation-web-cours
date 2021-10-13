ba63b2ca2674e2908bc4943929dd23ca701f9881
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 11:03:16 +0200

    Module 4 : La POO : Programmation Orientée Objets (en PHP), E2-Creer notre premiere classe

--- coursPHP/Exo4/common/menu.php
+++ coursPHP/Exo4/common/menu.php
@@ -2,5 +2,6 @@
     <ul>
         <li><a href="index.php">Accueil</a></li>
         <li><a href="personnage.php">Personnage</a></li>
+        <li><a href="personnageObjet.php">Personnage Objet</a></li>
     </ul>
 </nav>
