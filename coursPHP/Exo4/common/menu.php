5c5349f996394560533e58d84f5292be3ce7a535
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 11:39:53 +0200

    Module 4 : La POO : Programmation Orientée Objets (en PHP), E3-Fonction de classe

--- coursPHP/Exo4/common/menu.php
+++ coursPHP/Exo4/common/menu.php
@@ -2,6 +2,5 @@
     <ul>
         <li><a href="index.php">Accueil</a></li>
         <li><a href="personnage.php">Personnage</a></li>
-        <li><a href="personnageObjet.php">Personnage Objet</a></li>
     </ul>
 </nav>
--- coursPHP/Exo4/personnage.php
+++ coursPHP/Exo4/personnage.php
@@ -1,58 +1,68 @@
 <?php 
+
+class Personnage {
+    public $nom = "Luke";
+    public $img = "player.png";
+    public $age = 27;
+    public $sexe = true;
+    public $force = 5;
+    public $agilite = 4;
+
+    public function afficherMesInformations() {
+        echo "<b>Nom : </b>" . $this->nom . "<br/>";
+        echo "<b>Age : </b>" . $this->age . "<br/>";
+        echo "<b>Sexe : </b>";
+        if($this->sexe) {
+            echo "Homme <br/>";
+        } else {
+            echo "Femme <br/>";
+        }
+        echo "<b>Force : </b>" . $this->force . "<br/>";
+        echo "<b>Agilité : </b>" . $this->agilite . "<br/>";
+        echo "<br/>------------------------<br/>";
+    }
+
+    public function afficherMesInformationsModele() {
+        echo "<div class=\"gauche\">";
+            echo "<img src=\"sources/images/".$this->img."\" alt=\"".$this->img."\" />";
+        echo "</div>";
+        echo "<div class=\"gauche\">";
+        $this->afficherMesInformations();
+        echo "</div>";
+        echo "<div class=\"clearBoth\"></div>";
+    }
+}
+
     include("common/header.php");
     include("common/menu.php");
 
-    $tableauPersonnage1 = [
-        "Nom" => "Luke",
-        "Img" => "player.png",
-        "Age" => 27,
-        "Sexe" => true,
-        "Force" => 5,
-        "Agilite" => 4
-    ];
-    // $tableauPersonnage2 = [
-    //     "Nom" => "Katy",
-    //     "Img" => "playerF.png",
-    //     "Age" => 22,
-    //     "Sexe" => false,
-    //     "Force" => 3,
-    //     "Agilite" => 6
-    // ];
-    // $tableauPersonnage3 = [
-    //     "Nom" => "Marc",
-    //     "Img" => "playerM.png",
-    //     "Age" => 33,
-    //     "Sexe" => true,
-    //     "Force" => 7,
-    //     "Agilite" => 2
-    // ];
 ?>
 
 <h1> Personnage : </h1>
+
 <?php 
-    echo "<div class=\"gauche\">";
-        echo "<img src=\"sources/images/".$tableauPersonnage1['Img']."\" alt=\"".$tableauPersonnage1['Img']."\" />";
-    echo "</div>";
-    echo "<div class=\"gauche\">";
-        afficherPersonnage($tableauPersonnage1);
-    echo "</div>";
-    echo "<div class=\"clearBoth\"></div>";
-    
-    function afficherPersonnage($personnage) {
-        foreach($personnage as $index => $valeur) {
-            if($index !== "Img" && $index !== "Sexe"){
-                echo "<b>".$index."</b> : ". $valeur. "<br/>";
-            }
-            if($index === "Sexe") {
-                echo"<b>Sexe</b> : ";
-                if($valeur) {
-                    echo "Homme <br/>";
-                } else {
-                    echo "Femme <br/>";
-                }
-            }
-        }
-    }
+$personnage1 = new Personnage();
+$personnage1->afficherMesInformationsModele();
+
+$personnage2 = new Personnage();
+$personnage2->img = "playerF.png";
+$personnage2->nom = "Katy";
+$personnage2->age = 22;
+$personnage2->sexe = false;
+$personnage2->force = 3;
+$personnage2->agilite = 6;
+$personnage2->afficherMesInformationsModele();
+
+$personnage3 = new Personnage();
+$personnage3->img = "playerM.png";
+$personnage3->nom = "Marc";
+$personnage3->age = 33;
+$personnage3->sexe = true;
+$personnage3->force = 7;
+$personnage3->agilite = 2;
+
+$personnage3->afficherMesInformationsModele();
+
 ?>
 
 <?php 
