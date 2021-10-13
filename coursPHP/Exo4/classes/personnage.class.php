1bf253a91ab4008e7a22b1ac47b4462ca4fc75f0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 13:34:37 +0200

    Module 4 : La POO : Programmation Orientée Objets (en PHP), E6-Getter et Setter

--- coursPHP/Exo4/classes/personnage.class.php
+++ coursPHP/Exo4/classes/personnage.class.php
@@ -1,19 +1,67 @@
 <?php
 class Personnage {
-    public $nom;
-    public $image;
-    public $age;
-    public $sexe;
-    public $force;
-    public $agilite;
+    private $nom;
+    private $image;
+    private $age;
+    private $sexe;
+    private $force;
+    private $agilite;
 
-    public function __construct($nom,$image,$age,$sexe,$force,$agilite) {
+    function __construct($nom,$image,$age,$sexe,$force,$agilite) {
         $this->nom = $nom;
         $this->image = $image;
         $this->age = $age;
         $this->sexe = $sexe;
         $this->force = $force;
         $this->agilite = $agilite;
+    }
+
+    function getNom() {
+        return $this->nom;
+    }
+
+    function setNom($nom) {
+        return $this->nom = $nom;
+    }
+
+    function getImage() {
+        return $this->image;
+    }
+
+    function setImage($image) {
+        return $this->image = $image;
+    }
+
+    function getAge() {
+        return $this->age;
+    }
+
+    function setAge($age) {
+        return $this->age = $age;
+    }
+
+    function getSexe() {
+        return $this->sexe;
+    }
+
+    function setSexe($sexe) {
+        return $this->sexe = $sexe;
+    }
+
+    function getForce() {
+        return $this->force;
+    }
+
+    function setForce($force) {
+        return $this->force = $force;
+    }
+
+    function getAgilite() {
+        return $this->agilite;
+    }
+    
+    function setAgilite($agilite) {
+        return $this->agilite = $agilite;
     }
 
     public function afficherMesInformations() {
