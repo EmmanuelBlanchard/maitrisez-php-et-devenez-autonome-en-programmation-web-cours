419be87b6e065e4be383f489efbc00a8cd3f87eb
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 12:05:06 +0200

    Module 4 : La POO : Programmation Orientée Objets (en PHP), E5-Structuration du code

--- coursPHP/Exo4/personnage.php
+++ coursPHP/Exo4/personnage.php
@@ -1,50 +1,7 @@
 <?php 
-
-class Personnage {
-    public $nom;
-    public $image;
-    public $age;
-    public $sexe;
-    public $force;
-    public $agilite;
-
-    public function __construct($nom,$image,$age,$sexe,$force,$agilite) {
-        $this->nom = $nom;
-        $this->image = $image;
-        $this->age = $age;
-        $this->sexe = $sexe;
-        $this->force = $force;
-        $this->agilite = $agilite;
-    }
-
-    public function afficherMesInformations() {
-        echo "<b>Nom : </b>" . $this->nom . "<br/>";
-        echo "<b>Age : </b>" . $this->age . "<br/>";
-        echo "<b>Sexe : </b>";
-        if($this->sexe) {
-            echo "Homme <br/>";
-        } else {
-            echo "Femme <br/>";
-        }
-        echo "<b>Force : </b>" . $this->force . "<br/>";
-        echo "<b>Agilité : </b>" . $this->agilite . "<br/>";
-        echo "<br/>------------------------<br/>";
-    }
-
-    public function afficherMesInformationsModele() {
-        echo "<div class=\"gauche\">";
-            echo "<img src=\"sources/images/".$this->image."\" alt=\"".$this->image."\" />";
-        echo "</div>";
-        echo "<div class=\"gauche\">";
-        $this->afficherMesInformations();
-        echo "</div>";
-        echo "<div class=\"clearBoth\"></div>";
-    }
-}
-
+    require_once("classes/personnage.class.php");
     include("common/header.php");
     include("common/menu.php");
-
 ?>
 
 <h1> Personnage : </h1>
