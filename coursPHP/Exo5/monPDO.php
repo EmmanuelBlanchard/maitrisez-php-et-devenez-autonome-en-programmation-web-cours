e11038ada82afa83c6b01fba720fc8be6c7d3644
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-13 21:34:58 +0200

    Module 7 : PDO : Récupérer des données d'une BD, E2-Faire des requetes avec PDO (sans se peoccuper de la POO)

--- coursPHP/Exo5/monPDO.php
+++ coursPHP/Exo5/monPDO.php
@@ -6,13 +6,23 @@ const USER_NAME = "root";
 const PWD = "";
 
 try {
-    $connexion = 'mysql:host='.HOST_NAME.'; dbname = '.DB_NAME;
+    $connexion = 'mysql:host='.HOST_NAME.';dbname='.DB_NAME;
     $monPDO = new PDO($connexion,USER_NAME,PWD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch(PDOException $e){
     $message = "erreur de connexion à la DB". $e->getMessage();
     die($message);
 }
 
+if($monPDO){
+    $limitation = 130;
+    $req = "SELECT * FROM fruit WHERE Poids > :valeur";
+    $stmt = $monPDO->prepare($req);
+    $stmt->bindValue(':valeur', $limitation, PDO::PARAM_INT);
+    $stmt->execute();
+    $res1 = $stmt->fetchAll();
+    echo "<pre>";
+    print_r($res1);
+}
 
 
 ?>
